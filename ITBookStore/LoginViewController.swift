12c00b006f0e49512ac4521afc4fa19fcda3b913
Ankita Pabbi <erankitapabbi@gmail.com>
2019-07-17 14:15:29 -0400

    [Ankita Pabbi] Created a basic structure of application with login page layout and a tabbed layout.

--- ITBookStore/LoginViewController.swift
+++ ITBookStore/LoginViewController.swift
@@ -15,7 +15,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var txtEmail: UITextField!
     
     
-    @IBOutlet weak var txtPassword: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
