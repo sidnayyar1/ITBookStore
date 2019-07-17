08b37a244cc5762015bbc7e19b13b09c5b6425d9
Ankita Pabbi <erankitapabbi@gmail.com>
2019-07-17 14:21:35 -0400

    [Ankita Pabbi] Manipulating the login layout.

--- ITBookStore/LoginViewController.swift
+++ ITBookStore/LoginViewController.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 
-
-
+extension UITextField {
+    func setIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame:
+            CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame:
+            CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var txtEmail: UITextField!
+        {
+        didSet {
+            self.txtEmail.tintColor = UIColor.lightGray
+            if let image = UIImage(named: "usericon"){
+            self.txtEmail.setIcon(image)
+            }
+        }
+    }
     
-    
-    
-    
+    @IBOutlet weak var txtPassword: UITextField!
+        {
+        didSet {
+            self.txtPassword.tintColor = UIColor.lightGray
+            if let image = UIImage(named: "passwordicon"){
+                self.txtPassword.setIcon(image)
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
