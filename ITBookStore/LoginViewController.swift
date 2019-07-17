e3baaeb2c7fc517ab046f87d1d6830d9047faf73
Ankita Pabbi <erankitapabbi@gmail.com>
2019-07-17 14:13:15 -0400

    Initial Commit

---
+++ ITBookStore/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ITBookStore
+//
+//  Created by Ankita Pabbi on 2019-07-10.
+//  Copyright © 2019 Ankita Pabbi. All rights reserved.
+//
+
+import UIKit
+
+
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
